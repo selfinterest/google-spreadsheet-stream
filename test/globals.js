48223521cc2baf0d1d5f6050a866ddced0681a76
Terrence Watson <terrence.watson@contractor.cengage.com>
2014-09-28 19:24:32 -0400

    initial commit

--- test/globals.js
+++ test/globals.js
@@ -3,6 +3,8 @@
  */
 var chai = require("chai");
 var sinonChai = require("sinon-chai");
+var chaiAsPromised = require("chai-as-promised");
 global.sinon = require("sinon");
 chai.use(sinonChai);
+chai.use(chaiAsPromised);
 global.expect = chai.expect;
--- test/spreadsheetSpec.js
+++ test/spreadsheetSpec.js
@@ -2,6 +2,9 @@
  * Created by twatson on 9/28/14.
  */
 describe("spreadsheet constructor", function(){
+
+	var Q = require("q");
+
 	beforeEach(function(){
 		this.Spreadsheet = require("../lib/spreadsheet.js");
 	});
@@ -14,5 +17,24 @@ describe("spreadsheet constructor", function(){
 		expect(spreadsheet.names).to.not.be.undefined;
 	});
 
+	describe("init function", function(){
+		beforeEach(function(){
+			this.spreadsheet = new this.Spreadsheet();
+			this.spreadsheet.getSheetId = sinon.stub().returns(Q());
+			this.spreadsheet.setTemplates = sinon.stub();
+		});
 
+		it("should initialize, getting sheet ids and setting templates", function(done){
+			this.spreadsheet.init().then(function(){
+				expect(this.spreadsheet.getSheetId).to.have.been.calledTwice;
+				expect(this.spreadsheet.setTemplates).to.have.been.called;
+				done();
+			}.bind(this)).done();
+
+		});
+	});
+
+	describe("getSheetId function", function(){
+
+	});
 });
