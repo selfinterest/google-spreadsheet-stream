ce72b544f6b8ad1f309b42d5ac95d9318d906165
Terrence Watson <terrence.watson@contractor.cengage.com>
2014-10-02 11:07:54 -0400

    Stream implemented, basically.

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var parser = require("xml2json");
 var gsReader = gsStream
 	.email('759184919979-tfinm66j1hq49b3690039o8mfn60gfe3@developer.gserviceaccount.com')
 	.keyFile("./primary-documents-key-file.pem")
-	.spreadsheetId("1CgmFXfwRL1vuNb4y3JN42mkmWwB3tPQ_GLwJDGujXGc")
-	.worksheetId("o2xutm5")
+	.spreadsheetName("TestSpreadsheet")
+	.worksheetName("Sheet1")
 	.https(true)
 	.createStream()
 	;
@@ -20,12 +20,3 @@ gsReader.on("row", function(data){
 	console.log(data);
 });
 
-gsReader.on("error", function(){
-	console.log(arguments);
-});
-
-gsReader.on("end", function(data){
-	console.log("END");
-	var json = parser.toJson(allData, {object: true, sanitize: false});
-	//console.log(json.feed);
-});
