049236aca6aba4a2f207462825c45759e5b064de
Terrence Watson <terrence.watson@contractor.cengage.com>
2014-10-02 18:36:28 -0400

    Got a working example, too.

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 /**
  * Created by twatson on 10/1/14.
  */
-var gsStream = require("./lib/main.js").stream;
-var parser = require("xml2json");
-
-//spreadsheetId: "1CgmFXfwRL1vuNb4y3JN42mkmWwB3tPQ_GLwJDGujXGc",
-//	worksheetId: 'o2xutm5'
+var gsStream = require("./lib/main.js").stream, fs = require("fs"), through2 = require("through2"), _ = require("highland");
 
 var gsReader = gsStream
 	.email('759184919979-tfinm66j1hq49b3690039o8mfn60gfe3@developer.gserviceaccount.com')
@@ -17,6 +13,15 @@ var gsReader = gsStream
 	;
 
 gsReader.on("row", function(data){
-	console.log(data);
+	console.log(data.name);
 });
 
+gsReader.on("totalResults", function(total){
+
+});
+
+
+
+_(gsReader).map(function(obj){
+	return JSON.stringify(obj);
+}).pipe(fs.createWriteStream("./rows.txt"));
