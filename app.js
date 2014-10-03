55e158acc4fd11fad89e52d490e1ca7558d9481e
Terrence Watson <terrence.watson@contractor.cengage.com>
2014-10-03 19:02:14 -0400

    Cleaned code. Added ability to do structured queries using Google's API.

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 /**
  * Created by twatson on 10/1/14.
  */
-var gsStream = require("./lib/main.js").stream, fs = require("fs"), _ = require("highland");
+var gsStreamFactory = require("./lib/main.js").factory, fs = require("fs"), _ = require("highland");
 
-var gsReader = gsStream
+var gsReadStream = gsStreamFactory
 	.email('759184919979-tfinm66j1hq49b3690039o8mfn60gfe3@developer.gserviceaccount.com')
 	.keyFile("./primary-documents-key-file.pem")
 	.spreadsheetName("TestSpreadsheet")
 	.worksheetName("Sheet1")
 	.https(true)
-	.limit("1")         //return only 1 row
-	.offset("2")        //start at the second row
-	
+	//.limit(1)         //return only 1 row
+	///.offset(2)        //start at the second row
+	.query("name = terrence")
 	.createStream()
 	;
 
@@ -19,7 +19,7 @@ var gsReader = gsStream
 
 
 
-_(gsReader).map(function(obj){
+_(gsReadStream).map(function(obj){
 	console.log(obj);
 	return JSON.stringify(obj);
 }).pipe(fs.createWriteStream("./rows.txt"));
