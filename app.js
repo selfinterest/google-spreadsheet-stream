7d48ebb28bc918e041c9e98ec7d3454ed24c39f2
Terrence Watson <terrence.watson@contractor.cengage.com>
2014-10-03 22:24:09 -0400

    Added the ability to query Google

--- app.js
+++ app.js
@@ -6,18 +6,27 @@ var gsStreamFactory = require("./lib/main.js").factory, fs = require("fs"), _ = 
 var gsReadStream = gsStreamFactory
 	.email('759184919979-tfinm66j1hq49b3690039o8mfn60gfe3@developer.gserviceaccount.com')
 	.keyFile("./primary-documents-key-file.pem")
-	.spreadsheetName("TestSpreadsheet")
+	.spreadsheetName("PrimaryDocumentsTest")
 	.worksheetName("Sheet1")
 	.https(true)
-	//.limit(1)         //return only 1 row
+	.limit(1)         //return only 1 row
 	///.offset(2)        //start at the second row
-	.query("name = terrence")
+	//.query('name = Terrence')
 	.createStream()
 	;
 
 
+gsReadStream.on("rowCount", function(data){
+	console.log("This many rows: "+data);
+});
 
+gsReadStream.on("colCount", function(data){
+	console.log("This many columns: "+data);
+});
 
+gsReadStream.on("totalResults", function(data){
+	console.log("This many results: "+data);
+});
 
 _(gsReadStream).map(function(obj){
 	console.log(obj);
