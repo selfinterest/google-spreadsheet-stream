3d9260fe66b8103246101cc026156a7469f57377
Terrence Watson <terrence.watson@contractor.cengage.com>
2014-10-21 22:06:53 -0400

    Made some significant api changes/additions. Incremented version number and updated README to reflect.

--- app.js
+++ app.js
@@ -3,20 +3,33 @@
  */
 var gsStreamFactory = require("./lib/main.js").factory, fs = require("fs"), _ = require("highland");
 
+
+var express = require("express");
+var router = express.Router();
+var app = express();
+
+router.get("/", function(req, res){
+	gsReadStream.createStream().pipe(res);
+});
+
+app.use(router);
+app.listen(3000);
+
 var gsReadStream = gsStreamFactory
 	.email('759184919979-tfinm66j1hq49b3690039o8mfn60gfe3@developer.gserviceaccount.com')
 	.keyFile("./primary-documents-key-file.pem")
-	.spreadsheetId("1CgmFXfwRL1vuNb4y3JN42mkmWwB3tPQ_GLwJDGujXGc")
-	.worksheetId('o2xutm5')
+		.spreadsheetName("TestSpreadsheet")
+		.worksheetName("Sheet1")
+	//.spreadsheetId("1CgmFXfwRL1vuNb4y3JN42mkmWwB3tPQ_GLwJDGujXGc")
+	//.worksheetId('o2xutm5')
 	.https(true)
-	.limit(10)         //return only 1 row
+	.limit(10);         //return only 1 row
 	//.offset(2)        //start at the second row
 	//.query('name = Terrence') //only return rows where name is Terrence
-	.createStream()
-	;
 
 
-gsReadStream.on("rowCount", function(data){
+
+/*gsReadStream.on("rowCount", function(data){
 	console.log("This many rows: "+data);
 });
 
@@ -31,8 +44,9 @@ gsReadStream.on("totalResults", function(data){
 gsReadStream.on("end", function(){
 	console.log("All done");
 });
-
+*/
+/*
 _(gsReadStream).map(function(obj){
 	console.log(obj);
 	return JSON.stringify(obj);
-}).pipe(fs.createWriteStream("./rows.txt"));
+}).pipe(fs.createWriteStream("./rows.txt"));*/
