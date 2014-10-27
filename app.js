fcbfd0375e31787dcd6f68c31071aae29a369475
Terrence Watson <terrence.watson@contractor.cengage.com>
2014-10-27 15:41:29 -0400

    Made some significant api changes/additions. Incremented version number and updated README to reflect.

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ var router = express.Router();
 var app = express();
 
 router.get("/", function(req, res){
-	gsReadStream.createStream().pipe(res);
+	res.setHeader("Content-Type", "application/json; charset=UTF-8");
+	gsReadStream.createJsonStream()
+		.pipe(res)
+		.on("end", function(){
+			console.log("ENDING");
+			res.end();
+		});
+
+	/*_(gsReadStream.createJsonStream()).map(function(obj){
+		return JSON.stringify(obj);
+	}).pipe(res);*/
 });
 
 app.use(router);
@@ -18,8 +28,8 @@ app.listen(3000);
 var gsReadStream = gsStreamFactory
 	.email('759184919979-tfinm66j1hq49b3690039o8mfn60gfe3@developer.gserviceaccount.com')
 	.keyFile("./primary-documents-key-file.pem")
-		.spreadsheetName("TestSpreadsheet")
-		.worksheetName("Sheet1")
+	.spreadsheetName("TestSpreadsheet")
+	.worksheetName("Sheet1")
 	//.spreadsheetId("1CgmFXfwRL1vuNb4y3JN42mkmWwB3tPQ_GLwJDGujXGc")
 	//.worksheetId('o2xutm5')
 	.https(true)
