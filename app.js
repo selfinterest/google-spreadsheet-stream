05cc9d7e4f37c57932fa39417884439a7bcf01ce
Terrence <parliamentaryterrence@gmail.com>
2014-12-17 18:19:36 -0500

    Got JSON stream working!

--- app.js
+++ app.js
@@ -10,20 +10,12 @@ var app = express();
 
 router.get("/", function(req, res){
 	res.setHeader("Content-Type", "application/json; charset=UTF-8");
-	gsReadStream.createJsonStream()
-		.pipe(res)
-		.on("end", function(){
-			console.log("ENDING");
-			res.end();
-		});
+	gsReadStream.createJsonStream().pipe(res);
 
-	/*_(gsReadStream.createJsonStream()).map(function(obj){
-		return JSON.stringify(obj);
-	}).pipe(res);*/
 });
 
 app.use(router);
-app.listen(3000);
+app.listen(4000);
 
 var gsReadStream = gsStreamFactory
 	.email('759184919979-tfinm66j1hq49b3690039o8mfn60gfe3@developer.gserviceaccount.com')
@@ -32,31 +24,8 @@ var gsReadStream = gsStreamFactory
 	.worksheetName("Sheet1")
 	//.spreadsheetId("1CgmFXfwRL1vuNb4y3JN42mkmWwB3tPQ_GLwJDGujXGc")
 	//.worksheetId('o2xutm5')
-	.https(true)
-	.limit(10);         //return only 1 row
+	.https(true);
+	//.limit(10)         //return only 1 row
 	//.offset(2)        //start at the second row
 	//.query('name = Terrence') //only return rows where name is Terrence
 
-
-
-/*gsReadStream.on("rowCount", function(data){
-	console.log("This many rows: "+data);
-});
-
-gsReadStream.on("colCount", function(data){
-	console.log("This many columns: "+data);
-});
-
-gsReadStream.on("totalResults", function(data){
-	console.log("This many results: "+data);
-});
-
-gsReadStream.on("end", function(){
-	console.log("All done");
-});
-*/
-/*
-_(gsReadStream).map(function(obj){
-	console.log(obj);
-	return JSON.stringify(obj);
-}).pipe(fs.createWriteStream("./rows.txt"));*/
